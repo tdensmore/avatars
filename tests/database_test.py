9c31564d9941b87a1aacfbb11c9328531ab76454
Todd Densmore <469399+tdensmore@users.noreply.github.com>
2025-02-10 12:37:52 -0500

    Fixing tests

--- tests/database_test.py
+++ tests/database_test.py
@@ -6,18 +6,27 @@ from testcontainers.postgres import PostgresContainer
 @pytest.fixture(scope="session")
 def postgres_container():
     with PostgresContainer("postgres:latest") as postgres:
-        postgres.start()
         yield postgres
 
 def test_database_connection(postgres_container):
+    # conn = psycopg.connect(
+    #     dbname=postgres_container.get_container_host_port().split("/")[1],
+    #     #dbname=postgres_container.get_container_host_port().split("/")[1],
+    #     user="postgres",
+    #     password="password",
+    #     host=postgres_container.get_container_host_port().split(":")[0],
+    #     port=postgres_container.get_container_host_port().split(":")[1].split("/")[0]
+    # )
+
     conn = psycopg.connect(
-        dbname=postgres_container.get_container_host_port().split("/")[1],
-        #dbname=postgres_container.get_container_host_port().split("/")[1],
-        user="postgres",
-        password="password",
-        host=postgres_container.get_container_host_port().split(":")[0],
-        port=postgres_container.get_container_host_port().split(":")[1].split("/")[0]
+        host=postgres_container.get_container_host_ip(),
+        port=postgres_container.get_exposed_port(5432),
+        user=postgres_container.username,
+        password=postgres_container.password,
+        dbname=postgres_container.dbname
     )
+
+
     cur = conn.cursor()
     cur.execute("SELECT 1")
     result = cur.fetchone()
